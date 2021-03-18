8776baedf09f6ef03747c348002f67800221321a
Clement <cdreptin@gmail.com>
2021-03-18 16:23:11 +0100

    Added and implemented Spawn Care Package

--- Hayzen/src/Games/MW2/Functions.h
+++ Hayzen/src/Games/MW2/Functions.h
@@ -14,6 +14,8 @@ namespace MW2
 
 	extern int(__cdecl *Dvar_GetInt)(const char* dvarName);
 
+	extern const char* (_cdecl *Dvar_GetString)(const char* dvarName);
+
 	extern game_hudelem_s*(__cdecl *HudElem_Alloc)(int clientNum, int teamNum);
 
 	extern void(__cdecl *HudElem_Free)(game_hudelem_s* hud);
@@ -28,6 +30,18 @@ namespace MW2
 
 	extern bool(__cdecl *Session_IsHost)(DWORD sessionDataPtr, int clientNum);
 
+	extern void(__cdecl *SP_script_model)(gentity_s* mSelf);
+
+	extern gentity_s*(__cdecl *G_Spawn)();
+
+	extern void(__cdecl *G_SetModel)(gentity_s* ent, const char* modelName);
+
+	extern void(__cdecl *SV_LinkEntity)(gentity_s* gEnt);
+
+	extern void(__cdecl *SV_UnlinkEntity)(gentity_s* gEnt);
+
+	extern void(__cdecl *SV_SetBrushModel)(gentity_s* ent);
+
 	extern gclient_s* GetGClient(int clientNum);
 
 	extern gentity_s* GetPlayerEntity(int clientNum);
@@ -37,4 +51,6 @@ namespace MW2
 	extern void iPrintLn(int clientNum, const std::string& text);
 
 	extern bool IsHost(int clientNum);
+
+	extern gentity_s* GetCurrentMapEntity();
 }

f7de9312c13ab043196b5e34176b2153da55178f
Clement <cdreptin@gmail.com>
2021-03-18 16:37:49 +0100

    Declared the MW2 namespace globally

--- Hayzen/src/Games/MW2/Functions.cpp
+++ Hayzen/src/Games/MW2/Functions.cpp
@@ -4,105 +4,108 @@
 #include "Games\MW2\Structs.h"
 #include "Utils\Formatter.h"
 
-void(__cdecl *MW2::Cmd_RegisterNotification)(int clientNum, const char* notify, const char* command) = (void(*)(int, const char*, const char*))0x82225858;
-
-const char*(__cdecl *MW2::SL_ConvertToString)(unsigned int stringValue) = (const char*(*)(unsigned int))0x82241898;
-
-void(__cdecl *MW2::SV)(int clientNum, int type, const char* text) = (void(*)(int, int, const char*))0x822548D8;
-
-bool(__cdecl *MW2::Dvar_GetBool)(const char* dvarName) = (bool(*)(const char*))0x8229EEE8;
-
-int(__cdecl *MW2::Dvar_GetInt)(const char* dvarName) = (int(*)(const char*))0x8229EF50;
-
-const char*(__cdecl *MW2::Dvar_GetString)(const char* dvarName) = (const char*(*)(const char*))0x8229F0A8;
-
-MW2::game_hudelem_s*(__cdecl *MW2::HudElem_Alloc)(int clientNum, int teamNum) = (MW2::game_hudelem_s*(*)(int, int))0x821DF928;
-
-void(__cdecl *MW2::HudElem_Free)(MW2::game_hudelem_s* hud) = (void(*)(MW2::game_hudelem_s*))0x821DF9C0;
-
-int(__cdecl *MW2::G_MaterialIndex)(const char* name) = (int(*)(const char*))0x8220C960;
-
-int(__cdecl *MW2::G_LocalizedStringIndex)(const char* string) = (int(*)(const char*))0x8220C7A0;
-
-MW2::clientState_s*(__cdecl *MW2::GetClientState)(int clientNum) = (MW2::clientState_s*(*)(int))0x821E6610;
-
-MW2::playerState_s*(__cdecl *MW2::GetPlayerState)(int clientNum) = (MW2::playerState_s*(*)(int))0x821E6628;
-
-bool(__cdecl *MW2::Session_IsHost)(DWORD sessionDataPtr, int clientNum) = (bool(*)(DWORD, int))0x82320138;
-
-void(__cdecl *MW2::SP_script_model)(MW2::gentity_s* mSelf) = (void(*)(MW2::gentity_s*))0x82206D88;
-
-MW2::gentity_s*(__cdecl *MW2::G_Spawn)() = (MW2::gentity_s*(*)())0x8220DB50;
-
-void(__cdecl *MW2::G_SetModel)(MW2::gentity_s* ent, const char* modelName) = (void(*)(MW2::gentity_s*, const char*))0x8220D278;
-
-void(__cdecl *MW2::SV_LinkEntity)(MW2::gentity_s* gEnt) = (void(*)(MW2::gentity_s*))0x8225F518;
-
-void(__cdecl *MW2::SV_UnlinkEntity)(MW2::gentity_s* gEnt) = (void(*)(MW2::gentity_s*))0x8225F430;
-
-void(__cdecl *MW2::SV_SetBrushModel)(MW2::gentity_s* ent) = (void(*)(MW2::gentity_s*))0x82254B50;
-
-MW2::gclient_s* MW2::GetGClient(int clientNum)
+namespace MW2
 {
-	return (gclient_s*)(0x830CBF80 + sizeof(gclient_s) * clientNum);
-}
+	void(__cdecl *Cmd_RegisterNotification)(int clientNum, const char* notify, const char* command) = (void(*)(int, const char*, const char*))0x82225858;
 
-MW2::gentity_s* MW2::GetPlayerEntity(int clientNum)
-{
-	return (gentity_s*)(0x82F03600 + sizeof(gentity_s) * clientNum);
-}
+	const char*(__cdecl *SL_ConvertToString)(unsigned int stringValue) = (const char*(*)(unsigned int))0x82241898;
 
-void MW2::SetClientDvar(int clientNum, const std::string& dvar, const std::string& value)
-{
-	SV(clientNum, 0, Formatter::Format("s %s \"%s\"", dvar.c_str(), value.c_str()).c_str());
-}
+	void(__cdecl *SV)(int clientNum, int type, const char* text) = (void(*)(int, int, const char*))0x822548D8;
 
-void MW2::iPrintLn(int clientNum, const std::string& text)
-{
-	SV(clientNum, 0, Formatter::Format("f \"%s\"", text.c_str()).c_str());
-}
+	bool(__cdecl *Dvar_GetBool)(const char* dvarName) = (bool(*)(const char*))0x8229EEE8;
 
-bool MW2::IsHost(int clientNum)
-{
-	return Session_IsHost(0x83AC3DB0, clientNum);
-}
+	int(__cdecl *Dvar_GetInt)(const char* dvarName) = (int(*)(const char*))0x8229EF50;
 
-MW2::gentity_s* MW2::GetCurrentMapEntity()
-{
-	std::string mapName = Dvar_GetString("ui_mapname");
+	const char*(__cdecl *Dvar_GetString)(const char* dvarName) = (const char*(*)(const char*))0x8229F0A8;
 
-	if (mapName == "mp_afghan")
-		return (gentity_s*)0x82F7E800;
-	if (mapName == "mp_derail")
-		return (gentity_s*)0x82F5F680;
-	if (mapName == "mp_estate")
-		return (gentity_s*)0x82F4AF00;
-	if (mapName == "mp_favela")
-		return (gentity_s*)0x82F70C00;
-	if (mapName == "mp_highrise")
-		return (gentity_s*)0x82F76380;
-	if (mapName == "mp_invasion")
-		return (gentity_s*)0x82F73180;
-	if (mapName == "mp_checkpoint")
-		return (gentity_s*)0x82F6D280;
-	if (mapName == "mp_quarry")
-		return (gentity_s*)0x82F99600;
-	if (mapName == "mp_rundown")
-		return (gentity_s*)0x82F65800;
-	if (mapName == "mp_rust")
-		return (gentity_s*)0x82F3C900;
-	if (mapName == "mp_boneyard")
-		return (gentity_s*)0x82F0B300;
-	if (mapName == "mp_nightshift")
-		return (gentity_s*)0x82F3A880;
-	if (mapName == "mp_subbase")
-		return (gentity_s*)0x82F59780;
-	if (mapName == "mp_terminal")
-		return (gentity_s*)0x82F0DD80;
-	if (mapName == "mp_underpass")
-		return (gentity_s*)0x82F66C00;
-	if (mapName == "mp_brecourt")
-		return (gentity_s*)0x82F15580;
-	else
-		return nullptr;
+	game_hudelem_s*(__cdecl *HudElem_Alloc)(int clientNum, int teamNum) = (game_hudelem_s*(*)(int, int))0x821DF928;
+
+	void(__cdecl *HudElem_Free)(game_hudelem_s* hud) = (void(*)(game_hudelem_s*))0x821DF9C0;
+
+	int(__cdecl *G_MaterialIndex)(const char* name) = (int(*)(const char*))0x8220C960;
+
+	int(__cdecl *G_LocalizedStringIndex)(const char* string) = (int(*)(const char*))0x8220C7A0;
+
+	clientState_s*(__cdecl *GetClientState)(int clientNum) = (clientState_s*(*)(int))0x821E6610;
+
+	playerState_s*(__cdecl *GetPlayerState)(int clientNum) = (playerState_s*(*)(int))0x821E6628;
+
+	bool(__cdecl *Session_IsHost)(DWORD sessionDataPtr, int clientNum) = (bool(*)(DWORD, int))0x82320138;
+
+	void(__cdecl *SP_script_model)(gentity_s* mSelf) = (void(*)(gentity_s*))0x82206D88;
+
+	gentity_s*(__cdecl *G_Spawn)() = (gentity_s*(*)())0x8220DB50;
+
+	void(__cdecl *G_SetModel)(gentity_s* ent, const char* modelName) = (void(*)(gentity_s*, const char*))0x8220D278;
+
+	void(__cdecl *SV_LinkEntity)(gentity_s* gEnt) = (void(*)(gentity_s*))0x8225F518;
+
+	void(__cdecl *SV_UnlinkEntity)(gentity_s* gEnt) = (void(*)(gentity_s*))0x8225F430;
+
+	void(__cdecl *SV_SetBrushModel)(gentity_s* ent) = (void(*)(gentity_s*))0x82254B50;
+
+	gclient_s* GetGClient(int clientNum)
+	{
+		return (gclient_s*)(0x830CBF80 + sizeof(gclient_s) * clientNum);
+	}
+
+	gentity_s* GetPlayerEntity(int clientNum)
+	{
+		return (gentity_s*)(0x82F03600 + sizeof(gentity_s) * clientNum);
+	}
+
+	void SetClientDvar(int clientNum, const std::string& dvar, const std::string& value)
+	{
+		SV(clientNum, 0, Formatter::Format("s %s \"%s\"", dvar.c_str(), value.c_str()).c_str());
+	}
+
+	void iPrintLn(int clientNum, const std::string& text)
+	{
+		SV(clientNum, 0, Formatter::Format("f \"%s\"", text.c_str()).c_str());
+	}
+
+	bool IsHost(int clientNum)
+	{
+		return Session_IsHost(0x83AC3DB0, clientNum);
+	}
+
+	gentity_s* GetCurrentMapEntity()
+	{
+		std::string mapName = Dvar_GetString("ui_mapname");
+
+		if (mapName == "mp_afghan")
+			return (gentity_s*)0x82F7E800;
+		if (mapName == "mp_derail")
+			return (gentity_s*)0x82F5F680;
+		if (mapName == "mp_estate")
+			return (gentity_s*)0x82F4AF00;
+		if (mapName == "mp_favela")
+			return (gentity_s*)0x82F70C00;
+		if (mapName == "mp_highrise")
+			return (gentity_s*)0x82F76380;
+		if (mapName == "mp_invasion")
+			return (gentity_s*)0x82F73180;
+		if (mapName == "mp_checkpoint")
+			return (gentity_s*)0x82F6D280;
+		if (mapName == "mp_quarry")
+			return (gentity_s*)0x82F99600;
+		if (mapName == "mp_rundown")
+			return (gentity_s*)0x82F65800;
+		if (mapName == "mp_rust")
+			return (gentity_s*)0x82F3C900;
+		if (mapName == "mp_boneyard")
+			return (gentity_s*)0x82F0B300;
+		if (mapName == "mp_nightshift")
+			return (gentity_s*)0x82F3A880;
+		if (mapName == "mp_subbase")
+			return (gentity_s*)0x82F59780;
+		if (mapName == "mp_terminal")
+			return (gentity_s*)0x82F0DD80;
+		if (mapName == "mp_underpass")
+			return (gentity_s*)0x82F66C00;
+		if (mapName == "mp_brecourt")
+			return (gentity_s*)0x82F15580;
+		else
+			return nullptr;
+	}
 }
